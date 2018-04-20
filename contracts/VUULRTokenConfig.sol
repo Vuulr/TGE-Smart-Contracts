28d6bdbc5f2fa397aa735534e31638fc094001d9
Chris Drumgoole <chris.drumgoole@vuulr.com>
2018-04-20 13:01:03 +0800

    updated token supply to 1 billion from 200 million

--- contracts/VUULRTokenConfig.sol
+++ contracts/VUULRTokenConfig.sol
@@ -5,5 +5,5 @@ contract VUULRTokenConfig {
     string public constant SYMBOL = "VUU";
     uint8 public constant DECIMALS = 18;
     uint public constant DECIMALSFACTOR = 10 ** uint(DECIMALS);
-    uint public constant TOTALSUPPLY = 200000000 * DECIMALSFACTOR;
+    uint public constant TOTALSUPPLY = 1000000000 * DECIMALSFACTOR;
 }
